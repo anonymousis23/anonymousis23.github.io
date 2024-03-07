952b095c0da57f464e81e0cdf1aa73c6d1cc9b58
warisqr007 <quamer.waris@tamu.edu>
2024-03-07 11:45:23 -0600

    IS24

---
+++ IS24/User Study/scripts/create_html_ab.py
@@ -0,0 +1,107 @@
+from pathlib import Path
+import os
+import random
+
+out_dir = Path("/Users/warisqr/PSI/IS24/User Study/interfaces")
+audio_type_code = {
+    'same': 'SME',
+    'same_anon': 'SMA',
+    'different': 'DIF'
+}
+random.seed(1234)
+
+###########################################################################################################################
+#
+# VOICE SIMILARITY TEST (A B)
+#
+###########################################################################################################################
+
+audio_samples_root = "/Users/warisqr/PSI/IS24/User Study/samples/Verifiability"
+url_root = "<url>"
+
+
+wav_list = []
+wav_list = []
+_, audio_type_dirs, _ = next(os.walk(audio_samples_root))
+
+for audio_type in audio_type_dirs:
+    utterances = open(os.path.join(audio_samples_root, audio_type, 'info.txt'), 'r').readlines()
+    
+    for uttr_pair in utterances:
+        uttr_a, uttr_b = uttr_pair.strip().split('|')
+        wav_dict = {}
+        wav_dict['wav_fpath_a'] = f"{os.path.join(url_root, audio_type, uttr_a)}?raw=true"
+        wav_dict['wav_fpath_b'] = f"{os.path.join(url_root, audio_type, uttr_b)}?raw=true"
+        wav_dict['id'] = f"VS_{audio_type_code[audio_type]}_{Path(uttr_a).stem}_{Path(uttr_b).stem}"
+        wav_list.append(wav_dict)
+        
+
+random.shuffle(wav_list)
+test_samples = len(wav_list)
+
+voice_similarity_test_fpath = out_dir.joinpath("verifiability_test.html")
+voice_similarity_test_fpath = voice_similarity_test_fpath.open("w", encoding="utf-8")
+
+num_sets = test_samples//5
+for i in range(num_sets):
+    voice_similarity_test_fpath.write("<h2>Set "+str(i+1)+"</h2>" + "\n")
+    voice_similarity_test_fpath.write("<table class=\"tests\">" + "\n")
+    voice_similarity_test_fpath.write("\t" + "<tbody>" + "\n")
+    voice_similarity_test_fpath.write("\t\t" + "<tr>" + "\n")
+    voice_similarity_test_fpath.write("\t\t\t" + "<td><strong>QuestionID</strong></td>" + "\n")
+    voice_similarity_test_fpath.write("\t\t\t" + "<td><strong>&nbsp; &nbsp;Clip A &nbsp;&nbsp;</strong></td>" + "\n")
+    voice_similarity_test_fpath.write("\t\t\t" + "<td><strong>&nbsp; &nbsp;Clip B &nbsp;&nbsp;</strong></td>" + "\n")
+    voice_similarity_test_fpath.write("\t\t\t" + "<td><strong>Same or Not?</strong></td>" + "\n")
+    voice_similarity_test_fpath.write("\t\t\t" + "<td><strong>Not at all confident</strong></td>" + "\n")
+    voice_similarity_test_fpath.write("\t\t\t" + "<td><strong>&nbsp; &nbsp;| &nbsp;&nbsp;</strong></td>" + "\n")
+    voice_similarity_test_fpath.write("\t\t\t" + "<td><strong>Somewhat confident</strong></td>" + "\n")
+    voice_similarity_test_fpath.write("\t\t\t" + "<td><strong>&nbsp; &nbsp;| &nbsp;&nbsp;</strong></td>" + "\n")
+    voice_similarity_test_fpath.write("\t\t\t" + "<td><strong>Quite a bit confident</strong></td>" + "\n")
+    voice_similarity_test_fpath.write("\t\t\t" + "<td><strong>&nbsp; &nbsp;| &nbsp;&nbsp;</strong></td>" + "\n")
+    voice_similarity_test_fpath.write("\t\t\t" + "<td><strong>Extremely confident</strong></td>" + "\n")
+    voice_similarity_test_fpath.write("\t\t" + "</tr>" + "\n")
+
+    for j in range(5):
+        index = i*5+j
+        wav_id = wav_list[index]['id']
+        wav_fpath_a = wav_list[index]['wav_fpath_a']
+        wav_fpath_b = wav_list[index]['wav_fpath_b']
+        wav_fpath_x = wav_list[index]['wav_fpath_x']
+
+        voice_similarity_test_fpath.write("\t\t" + "<tr>" + "\n")
+
+        voice_similarity_test_fpath.write("\t\t\t" + "<td>Q"+str(index+1)+"</td>" + "\n")
+
+        voice_similarity_test_fpath.write("\t\t\t" + "<td>" + "\n")
+        voice_similarity_test_fpath.write("\t\t\t\t" + "<audio controls=\"controls\" preload=\"none\">" + "\n")
+        voice_similarity_test_fpath.write("\t\t\t\t\t" + "<source preload=\"none\" src=\"" + str(wav_fpath_a) + "\" type=\"audio/wav\" />" + "\n")
+        voice_similarity_test_fpath.write("\t\t\t\t" + "</audio>" + "\n")
+        voice_similarity_test_fpath.write("\t\t\t" + "</td>" + "\n")
+
+        voice_similarity_test_fpath.write("\t\t\t" + "<td>" + "\n")
+        voice_similarity_test_fpath.write("\t\t\t\t" + "<audio controls=\"controls\" preload=\"none\">" + "\n")
+        voice_similarity_test_fpath.write("\t\t\t\t\t" + "<source preload=\"none\" src=\"" + str(wav_fpath_b) + "\" type=\"audio/wav\" />" + "\n")
+        voice_similarity_test_fpath.write("\t\t\t\t" + "</audio>" + "\n")
+        voice_similarity_test_fpath.write("\t\t\t" + "</td>" + "\n")
+
+        voice_similarity_test_fpath.write("\t\t\t" + "<td>" + "\n")
+        voice_similarity_test_fpath.write("\t\t\t\t" + "<select class=\"form-control overall\" name=\"" + str(wav_id) + "_Answer_overall\">" + "\n")
+        voice_similarity_test_fpath.write("\t\t\t\t\t" + "<option selected=\"selected\" value=\"0\">- select one -</option>" + "\n")
+        voice_similarity_test_fpath.write("\t\t\t\t\t" + "<option value=\"1\">Same</option>" + "\n")
+        voice_similarity_test_fpath.write("\t\t\t\t\t" + "<option value=\"-1\">Different</option>" + "\n")
+        voice_similarity_test_fpath.write("\t\t\t\t" + "</select>" + "\n")  
+        voice_similarity_test_fpath.write("\t\t\t" + "</td>" + "\n")
+
+        voice_similarity_test_fpath.write("\t\t\t" + "<td style=\"text-align: center;\"><input name=\""+ str(wav_id) +"\" type=\"radio\" value=\"1\" /></td>" + "\n")
+        voice_similarity_test_fpath.write("\t\t\t" + "<td style=\"text-align: center;\"><input name=\""+ str(wav_id) +"\" type=\"radio\" value=\"2\" /></td>" + "\n")
+        voice_similarity_test_fpath.write("\t\t\t" + "<td style=\"text-align: center;\"><input name=\""+ str(wav_id) +"\" type=\"radio\" value=\"3\" /></td>" + "\n")
+        voice_similarity_test_fpath.write("\t\t\t" + "<td style=\"text-align: center;\"><input name=\""+ str(wav_id) +"\" type=\"radio\" value=\"4\" /></td>" + "\n")
+        voice_similarity_test_fpath.write("\t\t\t" + "<td style=\"text-align: center;\"><input name=\""+ str(wav_id) +"\" type=\"radio\" value=\"5\" /></td>" + "\n")
+        voice_similarity_test_fpath.write("\t\t\t" + "<td style=\"text-align: center;\"><input name=\""+ str(wav_id) +"\" type=\"radio\" value=\"6\" /></td>" + "\n")
+        voice_similarity_test_fpath.write("\t\t\t" + "<td style=\"text-align: center;\"><input name=\""+ str(wav_id) +"\" type=\"radio\" value=\"7\" /></td>" + "\n")
+
+        voice_similarity_test_fpath.write("\t\t" + "</tr>" + "\n")
+    voice_similarity_test_fpath.write("\t" + "</tbody>" + "\n")
+    voice_similarity_test_fpath.write("</table>" + "\n")
+
+voice_similarity_test_fpath.close()
---
+++ IS24/User Study/scripts/create_mos.py
@@ -0,0 +1,76 @@
+from pathlib import Path
+import os
+import random
+
+out_dir = Path("/Users/warisqr/PSI/IS24/User Study/interfaces")
+audio_type_code = {
+    'source': 'SR',
+    'base': 'BS',
+    'lite': 'LT'
+}
+random.seed(1234)
+
+# ##########################################################################################################################
+# MOS TEST
+# ##########################################################################################################################
+
+audio_samples_root = "/Users/warisqr/PSI/IS24/User Study/samples/MOS"
+url_root = "<url>"
+
+wav_list = []
+_, audio_type_dirs, _ = next(os.walk(audio_samples_root))
+
+for audio_type in audio_type_dirs:
+    _, _, utterance_ids = next(os.walk(os.path.join(audio_samples_root, audio_type)))
+    for utterance in utterance_ids:
+        wav_dict = {}
+        wav_dict['id'] = f"MOS_{audio_type_code[audio_type]}_{Path(utterance).stem}"
+        wav_dict['wav_fpath'] = f"{os.path.join(url_root, audio_type, utterance)}?raw=true"
+        wav_list.append(wav_dict)
+
+random.shuffle(wav_list)
+test_samples = len(wav_list)
+
+mos_test_fpath = out_dir.joinpath("mos_test.html")
+mos_test_fpath = mos_test_fpath.open("w", encoding="utf-8")
+
+num_sets = test_samples//5
+for i in range(num_sets):
+    mos_test_fpath.write("<h2>Set "+str(i+1)+"</h2>" + "\n")
+    mos_test_fpath.write("<table class=\"tests\">" + "\n")
+    mos_test_fpath.write("\t" + "<tbody>" + "\n")
+    mos_test_fpath.write("\t\t" + "<tr>" + "\n")
+    mos_test_fpath.write("\t\t\t" + "<td><strong>QuestionID</strong></td>" + "\n")
+    mos_test_fpath.write("\t\t\t" + "<td><strong>&nbsp; &nbsp;Clip &nbsp;&nbsp;</strong></td>" + "\n")
+    mos_test_fpath.write("\t\t\t" + "<td><strong>Not at all confident</strong></td>" + "\n")
+    mos_test_fpath.write("\t\t\t" + "<td><strong>&nbsp; &nbsp;| &nbsp;&nbsp;</strong></td>" + "\n")
+    mos_test_fpath.write("\t\t\t" + "<td><strong>Somewhat confident</strong></td>" + "\n")
+    mos_test_fpath.write("\t\t\t" + "<td><strong>&nbsp; &nbsp;| &nbsp;&nbsp;</strong></td>" + "\n")
+    mos_test_fpath.write("\t\t\t" + "<td><strong>Quite a bit confident</strong></td>" + "\n")
+    mos_test_fpath.write("\t\t" + "</tr>" + "\n")
+
+    for j in range(5):
+        index = i*5+j
+        wav_id = wav_list[index]['id']
+        wav_fpath = wav_list[index]['wav_fpath']
+
+        mos_test_fpath.write("\t\t" + "<tr>" + "\n")
+
+        mos_test_fpath.write("\t\t\t" + "<td>Q"+str(index+1)+"</td>" + "\n")
+
+        mos_test_fpath.write("\t\t\t" + "<td>" + "\n")
+        mos_test_fpath.write("\t\t\t\t" + "<audio controls=\"controls\" preload=\"none\">" + "\n")
+        mos_test_fpath.write("\t\t\t\t\t" + "<source preload=\"none\" src=\"" + str(wav_fpath) + "\" type=\"audio/wav\" />" + "\n")
+        mos_test_fpath.write("\t\t\t\t" + "</audio>" + "\n")
+        mos_test_fpath.write("\t\t\t" + "</td>" + "\n")
+
+        mos_test_fpath.write("\t\t\t" + "<td style=\"text-align: center;\"><input name=\""+ str(wav_id) +"\" type=\"radio\" value=\"1\" /></td>" + "\n")
+        mos_test_fpath.write("\t\t\t" + "<td style=\"text-align: center;\"><input name=\""+ str(wav_id) +"\" type=\"radio\" value=\"2\" /></td>" + "\n")
+        mos_test_fpath.write("\t\t\t" + "<td style=\"text-align: center;\"><input name=\""+ str(wav_id) +"\" type=\"radio\" value=\"3\" /></td>" + "\n")
+        mos_test_fpath.write("\t\t\t" + "<td style=\"text-align: center;\"><input name=\""+ str(wav_id) +"\" type=\"radio\" value=\"4\" /></td>" + "\n")
+        mos_test_fpath.write("\t\t\t" + "<td style=\"text-align: center;\"><input name=\""+ str(wav_id) +"\" type=\"radio\" value=\"5\" /></td>" + "\n")
+
+        mos_test_fpath.write("\t\t" + "</tr>" + "\n")
+    mos_test_fpath.write("\t" + "</tbody>" + "\n")
+    mos_test_fpath.write("</table>" + "\n")
+mos_test_fpath.close()
